281b83effb2c565587696c104ba6b8c894017d92
Alessandro Pezzè <igougi.ui@gmail.com>
2016-05-29 21:16:28 +0200

    gif 60fps

--- veekun.sh
+++ veekun.sh
@@ -2,7 +2,9 @@
 onlyAudio=false
 onlyMain=false
 onlyOther=false
-while getopts "zamog" flag
+onlyGif=false
+only60Fps=false
+while getopts "zamogf" flag
 do
   case $flag in
     z) zipSprites=true;;
@@ -10,10 +12,11 @@ do
     m) onlyMain=true;;
 	o) onlyOther=true;;
 	g) onlyGif=true;;
+	f) only60Fps=true;;
   esac
 done
 
-if [[ "$onlyOther" = "false" && "$onlyAudio" = "false" && "$onlyMain" = "false" && "$onlyGif" = "false" ]] ; then
+if [[ "$onlyOther" = "false" && "$onlyAudio" = "false" && "$onlyMain" = "false" && "$onlyGif" = "false" && "$only60Fps" = "false" ]] ; then
 	echo "Use some options"
 	exit
 fi
@@ -55,18 +58,24 @@ if [[ "$onlyGif" = "true" ]] ; then
 	dirs+=('http://sprites.pokecheck.org/s/')
 	dirs+=('http://sprites.pokecheck.org/bs/')
 fi
+if [[ "$only60Fps" = "true" ]] ; then
+	dirs+=('http://play.pokemonshowdown.com/sprites/xyani/')
+	dirs+=('http://play.pokemonshowdown.com/sprites/xyani-back/')
+	dirs+=('http://play.pokemonshowdown.com/sprites/xyani-shiny/')
+	dirs+=('http://play.pokemonshowdown.com/sprites/xyani-back-shiny/')
+fi
 
 FAIL=0
 for veek in "${veekun[@]}" ;
 do
 	echo "GET http://veekun.com/dex/media/pokemon/$veek/" &
-    wget --no-host-directories --no-cache -nc -c -U "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2600.0 Iron Safari/537.36" -r -l6 -A png,gif,svg,ogg -R html --no-parent http://veekun.com/dex/media/pokemon/$veek/ 1> NULL 2> NULL &
+    wget --no-host-directories --no-cache -nc -c -U "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2600.0 Iron Safari/537.36" -e robots=off -r -l6 -A png,gif,svg,ogg -R html,txt --no-parent http://veekun.com/dex/media/pokemon/$veek/ 1> NULL 2> NULL &
 done
 
 for dir in "${dirs[@]}" ;
 do
 	echo "GET $dir" &
-    wget -P gifs --no-host-directories --no-cache -nc -c -U "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2600.0 Iron Safari/537.36" -r -l1 -A gif -R html --no-parent --exclude-directories=/themes/ $dir/ 1> NULL 2> NULL &
+    wget -P gifs --no-host-directories --no-cache -nc -c -U "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2600.0 Iron Safari/537.36" -e robots=off -r -l1 -A gif -R html,txt --no-parent --exclude-directories=/themes/ $dir 1> NULL 2> NULL &
 done
 
 for job in `jobs -p`
